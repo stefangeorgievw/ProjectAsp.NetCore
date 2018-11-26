6c2d407c32e7448be3c0bee57f703a34958ef5e5
stefangeorgievw <stefangeorgiev30@gmail.com>
2018-11-26 15:21:26 +0200

    Implemented Project.Models/Contract.cs

--- Project.Models/Contract.cs
+++ Project.Models/Contract.cs
@@ -1,6 +1,20 @@
-﻿namespace Project.Models
+﻿using System;
+
+namespace Project.Models
 {
     public class Contract
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string UserId { get; set; }
+        public Account User { get; set; }
+
+        public string CompanyId { get; set; }
+        public Account Company { get; set; }
+
+        public string JobId { get; set; }
+        public Job Job { get; set; }
+
+        public DateTime DateOfCreation { get; set; } = DateTime.UtcNow;
     }
 }
