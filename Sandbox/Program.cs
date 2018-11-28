425fdd75045bae47c6e048d1377061982eafa7cc
stefangeorgievw <stefangeorgiev30@gmail.com>
2018-11-28 14:09:41 +0200

    Implemented Sandbox.csproj

---
+++ Sandbox/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project.Web.Data;
+
+namespace Sandbox
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+                SandboxCode(serviceProvider);
+            }
+        }
+
+        private static void SandboxCode(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetService<ApplicationDbContext>();
+            var user = context.Users.FirstOrDefault(x => x.Email == "stefangeorgievw@abv.bg");
+
+            Console.WriteLine(user.Email);
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            
+        }
+    }
+}
