3dab8ea250be5bac8b21eebef79cb741229fba9d
stefangeorgievw <stefangeorgiev30@gmail.com>
2018-12-23 16:18:10 +0200

    Implemented Offer.cs

---
+++ Project.Models/Offer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Models
+{
+    public class Offer
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string CompanyId { get; set; }
+        public CompanyProfile Company { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Comment { get; set; }
+
+        public string JobId { get; set; }
+        public Job Job { get; set; }
+    }
+}
