18b7194185ed68915d9d80139c63c2cc8fee7489
stefangeorgievw <stefangeorgiev30@gmail.com>
2019-01-04 00:21:50 +0200

    Implemented Rating

---
+++ Project.Models/Rating.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Models
+{
+    public class Rating
+    {
+        public Rating()
+        {
+            this.VotedUsers = new HashSet<UserRating>(); 
+        }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public double RatingSum { get; set; } = 0;
+
+        public double CountOfVotes { get; set; } 
+
+        public double AverageRating { get; set; }
+
+        public HashSet<UserRating> VotedUsers { get; set; } 
+
+    }
+}
---
+++ Project.Models/UserRating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Models
+{
+    public class UserRating
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
+
+        public string RatingId { get; set; }
+        public Rating Rating { get; set; }
+    }
+}
