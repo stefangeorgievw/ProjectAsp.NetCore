e02a79ad50a389e027e7b9c2e8af69e4f1417ddd
stefangeorgievw <stefangeorgiev30@gmail.com>
2019-01-08 13:59:09 +0200

    Added constants

--- Project.Common/Constants.cs
+++ Project.Common/Constants.cs
@@ -1,9 +1,20 @@
-﻿using System;
-
-namespace Project.Common
+﻿namespace Project.Common
 {
     public class Constants
     {
-        public static int companyDefaultRating = 0;
+        public const int companyDefaultRating = 0;
+        public const string userRoleName = "User";
+        public const string companyRoleName = "Company";
+        public const string adminRoleName = "Admin";
+        public const string homeUrl = "/";
+        public const string loginUrl = "/Account/Login";
+        public const string browseJobsUrl = "/Company/Job/Browse";
+        public const string myJobsUrl = "~/User/Job/MyJobs";
+        public const string contractName = "Contract";
+        public const string contractFormat = "application/pdf";
+        public const string myJobs = "MyJobs";
+        public const string homeIndexUrl = "~/Home/Index";
+        public const string userJobDetails = "~/User/Job/Details?id=";
+
     }
 }
